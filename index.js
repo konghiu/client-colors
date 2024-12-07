0fc91256b4d6468df62b777e375914fc4f4c24ed
konghiu <96055685+konghiu@users.noreply.github.com>
2024-12-07 16:08:08 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
+const URL = "https://iot-colors.vercel.app/colors/quantity";
+// const URL = "http://localhost:8000/colors/quantity";
+const RED = document.getElementById("red_quantity");
+const GREEN = document.getElementById("green_quantity");
+const BLUE = document.getElementById("blue_quantity");
+const OTHERS = document.getElementById("others_quantity");
+
 async function longPolling() {
     try {
-        const response = await fetch("https://iot-colors.vercel.app");
-        const data = await response.json();
-        console.log("Dữ liệu mới:", data);
+        await fetch(URL, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        })
+            .then((res) => {
+                console.log(res);
+                if (res.ok) return res.json();
+                return "conghieu";
+            })
+            .then((data) => {
+                console.log(data);
+                RED.innerHTML = data.red;
+                GREEN.innerHTML = data.green;
+                BLUE.innerHTML = data.blue;
+                OTHERS.innerHTML = data.others;
+            })
+            .finally(() => {
+                setTimeout(() => {
+                    longPolling();
+                }, 100);
+            });
     } catch (error) {
         console.error("Lỗi:", error);
+        setTimeout(longPolling, 3000);
     }
 }
 
