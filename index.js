ddf005e49c438bb917bdcee47a642109e6e5f614
konghiu <96055685+konghiu@users.noreply.github.com>
2024-12-07 14:03:44 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,8 @@ async function longPolling() {
         const response = await fetch("https://iot-colors.vercel.app");
         const data = await response.json();
         console.log("Dữ liệu mới:", data);
-        longPolling();
     } catch (error) {
         console.error("Lỗi:", error);
-        setTimeout(longPolling, 5000);
     }
 }
 
