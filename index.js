ff98158bc4c53601454ac4ed27ee650380e51f25
konghiu <96055685+konghiu@users.noreply.github.com>
2024-12-09 22:17:43 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const HOST = "https://iot-colors.vercel.app";
-// const HOST = "http://localhost:8000";
+// const HOST = "https://iot-colors.vercel.app";
+const HOST = "http://localhost:8000";
 const RED = document.getElementById("red_quantity");
 const GREEN = document.getElementById("green_quantity");
 const BLUE = document.getElementById("blue_quantity");
@@ -20,6 +20,8 @@ const NUMBER_PER_ALL = document.querySelector(".number_per_all");
 let list_details = [];
 let number_of_pages = 0;
 let current_page = 0;
+
+let icons_delete = [];
 
 let stop_get;
 
@@ -98,16 +100,16 @@ FILTER_DATE.onclick = async () => {
                 list_details = data;
                 number_of_pages = Math.ceil(list_details.length / 8);
                 current_page = 0;
-                let n = number_of_pages > 1 ? 8 : number_of_pages;
+                let n = number_of_pages > 1 ? 8 : list_details.length;
 
                 for (let i = 0; i < n; i++) {
-                    handle_create_detail_color(list_details[i]);
+                    handle_create_detail_color(list_details[i], i);
                 }
                 if (number_of_pages > 1) NAVBAR_PAGES.style.display = "flex";
                 else NAVBAR_PAGES.style.display = "none";
                 CONTAINER_QUANTITY.innerHTML = `<p>Có <b>${list_details.length}</b> sản phẩm</p>`;
-
                 NUMBER_PER_ALL.textContent = `1/${number_of_pages}`;
+                handle_delete_detail();
             })
             .catch((err) => {
                 console.log(err);
@@ -121,9 +123,10 @@ PAGE_PREV.onclick = () => {
     current_page -= 1;
     let n = current_page * 8 + 8;
     for (let i = current_page * 8; i < n; i++) {
-        handle_create_detail_color(list_details[i]);
+        handle_create_detail_color(list_details[i], i);
     }
     NUMBER_PER_ALL.textContent = `${current_page + 1}/${number_of_pages}`;
+    handle_delete_detail();
 };
 
 PAGE_NEXT.onclick = () => {
@@ -133,12 +136,13 @@ PAGE_NEXT.onclick = () => {
     n = n < list_details.length ? n : list_details.length;
     CONTAINER_DETAIL.innerHTML = "";
     for (let i = current_page * 8; i < n; i++) {
-        handle_create_detail_color(list_details[i]);
+        handle_create_detail_color(list_details[i], i);
     }
     NUMBER_PER_ALL.textContent = `${current_page + 1}/${number_of_pages}`;
+    handle_delete_detail();
 };
 
-function handle_create_detail_color(detail) {
+function handle_create_detail_color(detail, i) {
     const TIME_DETAIL = new Date(detail.time);
     let dd = TIME_DETAIL.getDate();
     dd = dd > 9 ? dd : "0" + dd;
@@ -164,11 +168,66 @@ function handle_create_detail_color(detail) {
     elementContent.innerHTML = `
         <h4>${detail.classified.classify}</h4>
         <p>${h}:${m}:${s} - ${dd}/${mm}/${yy}</p>
+        <span class="detail_delete" id=${detail._id}>x</span>
     `;
 
     const elementDetail = document.createElement("div");
     elementDetail.className = "detail_color";
+    elementDetail.id = `${i}`;
     elementDetail.appendChild(elementColor);
     elementDetail.appendChild(elementContent);
     CONTAINER_DETAIL.appendChild(elementDetail);
+}
+
+function handle_delete_detail() {
+    icons_delete = document.querySelectorAll(".detail_delete");
+    // console.log(icons_delete);
+
+    icons_delete.forEach((icon) => {
+        icon.onclick = async (e) => {
+            const ID = e.target.id;
+            const parent = e.target.parentElement.parentElement;
+            const idx = parent.id;
+            await fetch(HOST + "/colors/detail", {
+                method: "DELETE",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ id: ID }),
+            })
+                .then((res) => res.json())
+                .then((res) => {
+                    alert(res.message);
+
+                    list_details.splice(idx, 1);
+                    handle_reRender_detail();
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+            return handle_delete_detail();
+        };
+    });
+}
+
+function handle_reRender_detail() {
+    number_of_pages = Math.ceil(list_details.length / 8);
+
+    if (current_page >= number_of_pages) current_page -= 1;
+
+    let n = current_page * 8 + 8;
+    n = n < list_details.length ? n : list_details.length;
+
+    CONTAINER_DETAIL.innerHTML = "";
+    if (n > 0) {
+        for (let i = current_page * 8; i < n; i++) {
+            handle_create_detail_color(list_details[i], i);
+        }
+    }
+
+    if (number_of_pages > 1) NAVBAR_PAGES.style.display = "flex";
+    else NAVBAR_PAGES.style.display = "none";
+
+    CONTAINER_QUANTITY.innerHTML = `<p>Có <b>${list_details.length}</b> sản phẩm</p>`;
+    NUMBER_PER_ALL.textContent = `${current_page + 1}/${number_of_pages}`;
 }
